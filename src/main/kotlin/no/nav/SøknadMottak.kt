c4f7b0ae95c0d9b3aff510fa9032db0e4ee0872d
celinetendam <celine.ten.dam@nav.no>
2024-02-22 09:53:54 +0100

    Legger på nødvendig validering av felter i mottaket og tar vare på ident og søknadsData

--- src/main/kotlin/no/nav/SøknadMottak.kt
+++ src/main/kotlin/no/nav/SøknadMottak.kt
@@ -9,6 +9,10 @@ class SøknadMottak(rapidsConnection: RapidsConnection) : River.PacketListener {
     init {
         River(rapidsConnection).apply {
             validate { it.demandValue("@event_name", "innsending_ferdigstilt") }
+            validate { it.demandAny("type", listOf("NySøknad")) }
+            validate { it.requireKey("fødselsnummer") }
+            validate { it.requireKey("søknadsData") }
+            validate { it.interestedIn("@id", "@opprettet") }
         }.register(this)
     }
 
@@ -16,6 +20,8 @@ class SøknadMottak(rapidsConnection: RapidsConnection) : River.PacketListener {
         packet: JsonMessage,
         context: MessageContext,
     ) {
+        val ident = packet["fødselsnummer"].asText()
+        val søknadsData = packet["søknadsData"]
         context.publish("{}")
     }
 }
