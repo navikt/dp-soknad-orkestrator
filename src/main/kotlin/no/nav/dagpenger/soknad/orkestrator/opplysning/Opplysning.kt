5691a0c595cccc4047d236791a3be5ba9ec42ff7
celinetendam <celine.ten.dam@nav.no>
2024-03-27 10:50:39 +0100

    Legger til mappingfunksjon "tilOpplysning" i Datatype.

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/opplysning/Opplysning.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/opplysning/Opplysning.kt
@@ -1,5 +1,7 @@
 package no.nav.dagpenger.soknad.orkestrator.opplysning
 
+import com.fasterxml.jackson.databind.JsonNode
+import no.nav.helse.rapids_rivers.asLocalDate
 import java.time.LocalDate
 import java.util.UUID
 
@@ -11,22 +13,103 @@ data class Opplysning<T>(
     val søknadsId: UUID,
 )
 
-sealed class Datatype<T>(val klasse: Class<T>)
+sealed class Datatype<T>(val klasse: Class<T>) {
+    abstract fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*>
+}
 
-data object Tekst : Datatype<String>(String::class.java)
+data object Tekst : Datatype<String>(String::class.java) {
+    override fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*> {
+        val svar = faktum.get("svar").asText()
+        return Opplysning(beskrivendeId, Tekst, svar, ident, søknadId)
+    }
+}
 
-data object Heltall : Datatype<Int>(Int::class.java)
+data object Heltall : Datatype<Int>(Int::class.java) {
+    override fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*> {
+        val svar = faktum.get("svar").asInt()
+        return Opplysning(beskrivendeId, Heltall, svar, ident, søknadId)
+    }
+}
 
-data object Desimaltall : Datatype<Double>(Double::class.java)
+data object Desimaltall : Datatype<Double>(Double::class.java) {
+    override fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*> {
+        val svar = faktum.get("svar").asDouble()
+        return Opplysning(beskrivendeId, Desimaltall, svar, ident, søknadId)
+    }
+}
 
-data object Boolsk : Datatype<Boolean>(Boolean::class.java)
+data object Boolsk : Datatype<Boolean>(Boolean::class.java) {
+    override fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*> {
+        val svar = faktum.get("svar").asBoolean()
+        return Opplysning(beskrivendeId, Boolsk, svar, ident, søknadId)
+    }
+}
 
-data object Dato : Datatype<LocalDate>(LocalDate::class.java)
+data object Dato : Datatype<LocalDate>(LocalDate::class.java) {
+    override fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*> {
+        val svar = faktum.get("svar").asLocalDate()
+        return Opplysning(beskrivendeId, Dato, svar, ident, søknadId)
+    }
+}
 
 @Suppress("UNCHECKED_CAST")
-data object Flervalg : Datatype<List<String>>(String::class.java as Class<List<String>>)
+data object Flervalg : Datatype<List<String>>(String::class.java as Class<List<String>>) {
+    override fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*> {
+        val svar = faktum.get("svar").map { it.asText() }
+        return Opplysning(beskrivendeId, Flervalg, svar, ident, søknadId)
+    }
+}
 
-data object Periode : Datatype<PeriodeSvar>(PeriodeSvar::class.java)
+data object Periode : Datatype<PeriodeSvar>(PeriodeSvar::class.java) {
+    override fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*> {
+        val svar =
+            PeriodeSvar(
+                fom = faktum.get("svar").get("fom").asLocalDate(),
+                tom = faktum.get("svar").get("tom")?.asLocalDate(),
+            )
+        return Opplysning(beskrivendeId, Periode, svar, ident, søknadId)
+    }
+}
 
 data class PeriodeSvar(
     val fom: LocalDate,
@@ -36,7 +119,34 @@ data class PeriodeSvar(
 @Suppress("UNCHECKED_CAST")
 data object Arbeidsforhold : Datatype<List<ArbeidsforholdSvar>>(
     List::class.java as Class<List<ArbeidsforholdSvar>>,
-)
+) {
+    override fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*> {
+        val arbeidsforholdSvar: List<ArbeidsforholdSvar> =
+            faktum.get("svar").map { arbeidsforhold ->
+                // TODO: Hvordan vil vi håndtere null her?
+                val navnSvar =
+                    arbeidsforhold
+                        .find { it.get("beskrivendeId").asText() == "faktum.arbeidsforhold.navn-bedrift" }
+                        ?.get("svar")?.asText() ?: ""
+
+                val landFaktum =
+                    arbeidsforhold
+                        .find { it.get("beskrivendeId").asText() == "faktum.arbeidsforhold.land" }
+                        ?.get("svar")?.asText() ?: ""
+
+                ArbeidsforholdSvar(
+                    navn = navnSvar,
+                    land = landFaktum,
+                )
+            }
+        return Opplysning(beskrivendeId, Arbeidsforhold, arbeidsforholdSvar, ident, søknadId)
+    }
+}
 
 data class ArbeidsforholdSvar(
     val navn: String,
@@ -46,7 +156,48 @@ data class ArbeidsforholdSvar(
 @Suppress("UNCHECKED_CAST")
 data object EøsArbeidsforhold : Datatype<List<EøsArbeidsforholdSvar>>(
     List::class.java as Class<List<EøsArbeidsforholdSvar>>,
-)
+) {
+    override fun tilOpplysning(
+        faktum: JsonNode,
+        beskrivendeId: String,
+        ident: String,
+        søknadId: UUID,
+    ): Opplysning<*> {
+        val eøsArbeidsforholdSvar: List<EøsArbeidsforholdSvar> =
+            faktum.get("svar").map { eøsArbeidsforhold ->
+                val arbeidsgivernavnSvar =
+                    eøsArbeidsforhold
+                        .find { it.get("beskrivendeId").asText() == "faktum.eos-arbeidsforhold.arbeidsgivernavn" }
+                        ?.get("svar")?.asText() ?: ""
+
+                val landSvar =
+                    eøsArbeidsforhold
+                        .find { it.get("beskrivendeId").asText() == "faktum.eos-arbeidsforhold.land" }
+                        ?.get("svar")?.asText() ?: ""
+
+                val personnummerSvar =
+                    eøsArbeidsforhold
+                        .find { it.get("beskrivendeId").asText() == "faktum.eos-arbeidsforhold.personnummer" }
+                        ?.get("svar")?.asText() ?: ""
+
+                val varighet =
+                    eøsArbeidsforhold
+                        .find { it.get("beskrivendeId").asText() == "faktum.eos-arbeidsforhold.varighet" }
+                        ?.get("svar")
+
+                val fom = varighet?.get("fom")?.asLocalDate() ?: throw IllegalArgumentException("Fom dato mangler")
+                val tom = varighet.get("tom")?.asLocalDate()
+
+                EøsArbeidsforholdSvar(
+                    bedriftnavn = arbeidsgivernavnSvar,
+                    land = landSvar,
+                    personnummerIArbeidsland = personnummerSvar,
+                    varighet = PeriodeSvar(fom, tom),
+                )
+            }
+        return Opplysning(beskrivendeId, EøsArbeidsforhold, eøsArbeidsforholdSvar, ident, søknadId)
+    }
+}
 
 data class EøsArbeidsforholdSvar(
     val bedriftnavn: String,
--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/SøknadMapper.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/SøknadMapper.kt
@@ -2,20 +2,17 @@ package no.nav.dagpenger.soknad.orkestrator.søknad
 
 import com.fasterxml.jackson.databind.JsonNode
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Arbeidsforhold
-import no.nav.dagpenger.soknad.orkestrator.opplysning.ArbeidsforholdSvar
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Boolsk
+import no.nav.dagpenger.soknad.orkestrator.opplysning.Datatype
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Dato
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Desimaltall
 import no.nav.dagpenger.soknad.orkestrator.opplysning.EøsArbeidsforhold
-import no.nav.dagpenger.soknad.orkestrator.opplysning.EøsArbeidsforholdSvar
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Flervalg
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Heltall
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Opplysning
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Periode
-import no.nav.dagpenger.soknad.orkestrator.opplysning.PeriodeSvar
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Tekst
 import no.nav.dagpenger.soknad.orkestrator.utils.asUUID
-import no.nav.helse.rapids_rivers.asLocalDate
 import java.util.UUID
 
 class SøknadMapper(private val jsonNode: JsonNode) {
@@ -59,173 +56,47 @@ class SøknadMapper(private val jsonNode: JsonNode) {
             val fakta = seksjon.get("fakta") ?: throw IllegalArgumentException("Mangler fakta")
             fakta.asIterable().map { faktum ->
                 val beskrivendeId = faktum.get("beskrivendeId").asText()
-                val type = faktum.get("type").asText()
+                val faktumtype = faktum.get("type").asText()
 
-                val opplysning =
-                    when (type) {
-                        "land", "tekst", "envalg" ->
-                            Opplysning(
-                                beskrivendeId = beskrivendeId,
-                                type = Tekst,
-                                svar = faktum.get("svar").asText(),
-                                ident = ident,
-                                søknadsId = søknadId,
-                            )
-
-                        "int" ->
-                            Opplysning(
-                                beskrivendeId = beskrivendeId,
-                                type = Heltall,
-                                svar = faktum.get("svar").asInt(),
-                                ident = ident,
-                                søknadsId = søknadId,
-                            )
-
-                        "double" ->
-                            Opplysning(
-                                beskrivendeId = beskrivendeId,
-                                type = Desimaltall,
-                                svar = faktum.get("svar").asDouble(),
-                                ident = ident,
-                                søknadsId = søknadId,
-                            )
-
-                        "boolean" ->
-                            Opplysning(
-                                beskrivendeId = beskrivendeId,
-                                type = Boolsk,
-                                svar = faktum.get("svar").asBoolean(),
-                                ident = ident,
-                                søknadsId = søknadId,
-                            )
-
-                        "localdate" ->
-                            Opplysning(
-                                beskrivendeId = beskrivendeId,
-                                type = Dato,
-                                svar = faktum.get("svar").asLocalDate(),
-                                ident = ident,
-                                søknadsId = søknadId,
-                            )
-
-                        "flervalg" ->
-                            Opplysning(
-                                beskrivendeId = beskrivendeId,
-                                type = Flervalg,
-                                svar = faktum.get("svar").map { it.asText() },
-                                ident = ident,
-                                søknadsId = søknadId,
-                            )
-
-                        "periode" ->
-                            Opplysning(
-                                beskrivendeId,
-                                Periode,
-                                periodeSvar(faktum.get("svar")),
-                                ident,
-                                søknadId,
-                            )
-
-                        "generator" -> generatorSvar(faktum, ident, søknadId)
-
-                        else -> throw IllegalArgumentException("Ukjent faktumtype: $type")
-                    }
-                opplysning
+                val datatype: Datatype<*> = finnDatatype(faktumtype, beskrivendeId)
+                datatype.tilOpplysning(faktum, beskrivendeId, ident, søknadId)
             }
         }.flatten()
     }
-}
 
-private fun periodeSvar(periodesvar: JsonNode?): PeriodeSvar {
-    return periodesvar?.let {
-        PeriodeSvar(
-            fom = periodesvar.get("fom").asLocalDate(),
-            tom = periodesvar.get("tom")?.asLocalDate(),
-        )
-    } ?: throw IllegalArgumentException("Mangler 'svar' i periodefaktum")
-}
-
-private fun generatorSvar(
-    faktum: JsonNode,
-    ident: String,
-    søknadId: UUID,
-): Opplysning<*> {
-    val beskrivendeId = faktum.get("beskrivendeId").asText()
-    return when (beskrivendeId) {
-        "faktum.arbeidsforhold" -> tilArbeidsforholdOpplysning(faktum, beskrivendeId, ident, søknadId)
-
-        "faktum.eos-arbeidsforhold" -> tilEøsArbeidsforholdOpplysning(faktum, beskrivendeId, ident, søknadId)
-
-        // TODO: Håndter resten av generatorfaktumene
-        // "faktum.egen-naering-organisasjonsnummer-liste" ->
-        // "faktum.register.barn-liste" ->
-        // "faktum.barn-liste" ->
-
-        else -> throw IllegalArgumentException("Ukjent generatorfaktum: $beskrivendeId")
+    private fun finnDatatype(
+        type: String,
+        beskrivendeId: String?,
+    ): Datatype<*> {
+        val datatype: Datatype<*> =
+            if (type == "generator") {
+                when (beskrivendeId) {
+                    "faktum.arbeidsforhold" -> datatyper.getValue("arbeidsforhold")
+                    "faktum.eos-arbeidsforhold" -> datatyper.getValue("eøsArbeidsforhold")
+                    // TODO: Håndter resten av generatorfaktumene
+                    // "faktum.egen-naering-organisasjonsnummer-liste"
+                    // "faktum.register.barn-liste"
+                    // "faktum.barn-liste"
+                    else -> throw IllegalArgumentException("Ukjent generator")
+                }
+            } else {
+                datatyper.getValue(type)
+            }
+        return datatype
     }
-}
 
-private fun tilArbeidsforholdOpplysning(
-    faktum: JsonNode,
-    beskrivendeId: String,
-    ident: String,
-    søknadId: UUID,
-): Opplysning<List<ArbeidsforholdSvar>> {
-    val arbeidsforholdSvar: List<ArbeidsforholdSvar> =
-        faktum.get("svar").map { arbeidsforhold ->
-            // TODO: Hvordan vil vi håndtere null her?
-            val navnSvar =
-                arbeidsforhold
-                    .find { it.get("beskrivendeId").asText() == "faktum.arbeidsforhold.navn-bedrift" }
-                    ?.get("svar")?.asText() ?: ""
-
-            val landFaktum =
-                arbeidsforhold
-                    .find { it.get("beskrivendeId").asText() == "faktum.arbeidsforhold.land" }
-                    ?.get("svar")?.asText() ?: ""
-
-            ArbeidsforholdSvar(
-                navn = navnSvar,
-                land = landFaktum,
-            )
-        }
-    return Opplysning(beskrivendeId, Arbeidsforhold, arbeidsforholdSvar, ident, søknadId)
-}
-
-fun tilEøsArbeidsforholdOpplysning(
-    faktum: JsonNode,
-    beskrivendeId: String,
-    ident: String,
-    søknadId: UUID,
-): Opplysning<List<EøsArbeidsforholdSvar>> {
-    val eøsArbeidsforholdSvar: List<EøsArbeidsforholdSvar> =
-        faktum.get("svar").map { eøsArbeidsforhold ->
-            val arbeidsgivernavnSvar =
-                eøsArbeidsforhold
-                    .find { it.get("beskrivendeId").asText() == "faktum.eos-arbeidsforhold.arbeidsgivernavn" }
-                    ?.get("svar")?.asText() ?: ""
-
-            val landSvar =
-                eøsArbeidsforhold
-                    .find { it.get("beskrivendeId").asText() == "faktum.eos-arbeidsforhold.land" }
-                    ?.get("svar")?.asText() ?: ""
-
-            val personnummerSvar =
-                eøsArbeidsforhold
-                    .find { it.get("beskrivendeId").asText() == "faktum.eos-arbeidsforhold.personnummer" }
-                    ?.get("svar")?.asText() ?: ""
-
-            val varighetSvar =
-                eøsArbeidsforhold
-                    .find { it.get("beskrivendeId").asText() == "faktum.eos-arbeidsforhold.varighet" }
-                    ?.get("svar")
-
-            EøsArbeidsforholdSvar(
-                bedriftnavn = arbeidsgivernavnSvar,
-                land = landSvar,
-                personnummerIArbeidsland = personnummerSvar,
-                varighet = periodeSvar(varighetSvar),
-            )
-        }
-    return Opplysning(beskrivendeId, EøsArbeidsforhold, eøsArbeidsforholdSvar, ident, søknadId)
+    private val datatyper: Map<String, Datatype<*>> =
+        mapOf(
+            "tekst" to Tekst,
+            "land" to Tekst,
+            "envalg" to Tekst,
+            "int" to Heltall,
+            "double" to Desimaltall,
+            "boolean" to Boolsk,
+            "localdate" to Dato,
+            "flervalg" to Flervalg,
+            "periode" to Periode,
+            "arbeidsforhold" to Arbeidsforhold,
+            "eøsArbeidsforhold" to EøsArbeidsforhold,
+        )
 }
