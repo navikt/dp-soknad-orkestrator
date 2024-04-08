2268f0b4e3245ddadde0d75a15e9a0c4d0bd4723
Frode Sjovatsen <frode.sjovatsen@nav.no>
2024-04-08 10:23:35 +0200

    #$%& 🤬
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>
    Co-authored-by:  Abdifatah Bashi <abdifatah.bashi@nav.no>

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/config/ApiConfig.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/config/ApiConfig.kt
@@ -1,6 +1,5 @@
 package no.nav.dagpenger.soknad.orkestrator.config
 
-
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.callloging.CallLogging
@@ -8,7 +7,6 @@ import io.ktor.server.request.path
 import org.slf4j.event.Level
 
 internal fun Application.konfigurerApi() {
-
     install(CallLogging) {
         disableDefaultColors()
         filter {
