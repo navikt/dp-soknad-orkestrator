0848f820fe7f8ae16076e227ba0a2336477c3ae3
celinetendam <celine.ten.dam@nav.no>
2024-04-10 15:18:44 +0200

    Enabler promotheus i nais.yaml og setter op Micrometer Metrics.
    Co-authored-by: Yrjan Fraschetti <yrjan.fraschetti@nav.no

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/config/ApiConfig.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/config/ApiConfig.kt
@@ -1,9 +1,15 @@
 package no.nav.dagpenger.soknad.orkestrator.config
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.request.path
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.slf4j.event.Level
 
 internal fun Application.apiKonfigurasjon() {
@@ -13,5 +19,12 @@ internal fun Application.apiKonfigurasjon() {
             it.request.path() !in setOf("/metrics", "/isalive", "/isready")
         }
         level = Level.INFO
+    }
+
+    val micrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+    routing {
+        get("/metrics") {
+            call.respond(micrometerRegistry.scrape())
+        }
     }
 }
