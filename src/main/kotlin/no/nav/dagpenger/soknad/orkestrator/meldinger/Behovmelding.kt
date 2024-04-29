3702f44b71bb408e0c9f5f7a313ca2470d124e78
celinetendam <celine.ten.dam@nav.no>
2024-04-29 14:55:28 +0200

    Fjerner ubrukt variabel.
    Co-authored-by: Yrjan Fraschetti <yrjan.fraschetti@nav.no

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/meldinger/Behovmelding.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/meldinger/Behovmelding.kt
@@ -2,10 +2,8 @@ package no.nav.dagpenger.soknad.orkestrator.meldinger
 
 import no.nav.dagpenger.soknad.orkestrator.utils.asUUID
 import no.nav.helse.rapids_rivers.JsonMessage
-import java.util.UUID
 
 class Behovmelding(packet: JsonMessage) {
-    internal val id: UUID = UUID.fromString(packet["@id"].asText())
     val behov = packet["@behov"].map { it.asText() }
     val ident = packet["ident"].asText()
     val søknadId = packet["søknadId"].asUUID()
