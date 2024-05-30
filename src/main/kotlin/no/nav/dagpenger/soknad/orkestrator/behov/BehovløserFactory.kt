5e087c18f635ad0feb4194b562c248169cd2f50c
celinetendam <celine.ten.dam@nav.no>
2024-05-30 10:41:47 +0200

    Legg til behovløser for behov TarUtdanningEllerOpplæring

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/BehovløserFactory.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/BehovløserFactory.kt
@@ -1,5 +1,20 @@
 package no.nav.dagpenger.soknad.orkestrator.behov
 
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.EøsArbeid
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.HelseTilAlleTyperJobb
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.JobbetUtenforNorge
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.KanJobbeDeltid
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.KanJobbeHvorSomHelst
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.Lønnsgaranti
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.Ordinær
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.Permittert
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.PermittertFiskeforedling
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.Søknadsdato
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.Søknadstidspunkt
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.TarUtdanningEllerOpplæring
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.Verneplikt
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.VilligTilÅBytteYrke
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.ØnskerDagpengerFraDato
 import no.nav.dagpenger.soknad.orkestrator.behov.løsere.EøsArbeidBehovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.løsere.HelseTilAlleTyperJobbBehovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.løsere.JobbetUtenforNorgeBehovløser
@@ -11,6 +26,7 @@ import no.nav.dagpenger.soknad.orkestrator.behov.løsere.PermittertBehovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.løsere.PermittertFiskeforedlingBehovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.løsere.SøknadsdatoBehovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.løsere.SøknadstidspunktBehovløser
+import no.nav.dagpenger.soknad.orkestrator.behov.løsere.UtdanningEllerOpplæringBehovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.løsere.VernepliktBehovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.løsere.VilligTilÅBytteYrkeBehovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.løsere.ØnskerDagpengerFraDatoBehovløser
@@ -23,20 +39,21 @@ class BehovløserFactory(
 ) {
     private val behovløsere: Map<Behov, Behovløser> =
         mapOf(
-            Behov.ØnskerDagpengerFraDato to ØnskerDagpengerFraDatoBehovløser(rapidsConnection, opplysningRepository),
-            Behov.EøsArbeid to EøsArbeidBehovløser(rapidsConnection, opplysningRepository),
-            Behov.KanJobbeDeltid to KanJobbeDeltidBehovløser(rapidsConnection, opplysningRepository),
-            Behov.HelseTilAlleTyperJobb to HelseTilAlleTyperJobbBehovløser(rapidsConnection, opplysningRepository),
-            Behov.KanJobbeHvorSomHelst to KanJobbeHvorSomHelstBehovløser(rapidsConnection, opplysningRepository),
-            Behov.VilligTilÅBytteYrke to VilligTilÅBytteYrkeBehovløser(rapidsConnection, opplysningRepository),
-            Behov.Søknadstidspunkt to SøknadstidspunktBehovløser(rapidsConnection, opplysningRepository),
-            Behov.JobbetUtenforNorge to JobbetUtenforNorgeBehovløser(rapidsConnection, opplysningRepository),
-            Behov.Verneplikt to VernepliktBehovløser(rapidsConnection, opplysningRepository),
-            Behov.Lønnsgaranti to LønnsgarantiBehovløser(rapidsConnection, opplysningRepository),
-            Behov.Permittert to PermittertBehovløser(rapidsConnection, opplysningRepository),
-            Behov.PermittertFiskeforedling to PermittertFiskeforedlingBehovløser(rapidsConnection, opplysningRepository),
-            Behov.Ordinær to OrdinærBehovløser(rapidsConnection, opplysningRepository),
-            Behov.Søknadsdato to SøknadsdatoBehovløser(rapidsConnection, opplysningRepository),
+            ØnskerDagpengerFraDato to ØnskerDagpengerFraDatoBehovløser(rapidsConnection, opplysningRepository),
+            EøsArbeid to EøsArbeidBehovløser(rapidsConnection, opplysningRepository),
+            KanJobbeDeltid to KanJobbeDeltidBehovløser(rapidsConnection, opplysningRepository),
+            HelseTilAlleTyperJobb to HelseTilAlleTyperJobbBehovløser(rapidsConnection, opplysningRepository),
+            KanJobbeHvorSomHelst to KanJobbeHvorSomHelstBehovløser(rapidsConnection, opplysningRepository),
+            VilligTilÅBytteYrke to VilligTilÅBytteYrkeBehovløser(rapidsConnection, opplysningRepository),
+            Søknadstidspunkt to SøknadstidspunktBehovløser(rapidsConnection, opplysningRepository),
+            JobbetUtenforNorge to JobbetUtenforNorgeBehovløser(rapidsConnection, opplysningRepository),
+            Verneplikt to VernepliktBehovløser(rapidsConnection, opplysningRepository),
+            Lønnsgaranti to LønnsgarantiBehovløser(rapidsConnection, opplysningRepository),
+            Permittert to PermittertBehovløser(rapidsConnection, opplysningRepository),
+            PermittertFiskeforedling to PermittertFiskeforedlingBehovløser(rapidsConnection, opplysningRepository),
+            Ordinær to OrdinærBehovløser(rapidsConnection, opplysningRepository),
+            Søknadsdato to SøknadsdatoBehovløser(rapidsConnection, opplysningRepository),
+            TarUtdanningEllerOpplæring to UtdanningEllerOpplæringBehovløser(rapidsConnection, opplysningRepository),
         )
 
     fun behovløserFor(behov: Behov): Behovløser {
@@ -60,5 +77,6 @@ class BehovløserFactory(
         PermittertFiskeforedling,
         Ordinær,
         Søknadsdato,
+        TarUtdanningEllerOpplæring,
     }
 }
