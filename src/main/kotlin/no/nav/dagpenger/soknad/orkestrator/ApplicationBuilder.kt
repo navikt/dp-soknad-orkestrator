f7f229c6292246d12ce14c8389167dfdd3542c16
celinetendam <celine.ten.dam@nav.no>
2024-05-31 10:03:27 +0200

    Fjern CLEAN_ON_STARTUP fra nais.yaml og fjern koden som sjekker denne

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/ApplicationBuilder.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/ApplicationBuilder.kt
@@ -1,13 +1,11 @@
 package no.nav.dagpenger.soknad.orkestrator
 
 import mu.KotlinLogging
-import no.nav.dagpenger.soknad.orkestrator.PostgresDataSourceBuilder.clean
 import no.nav.dagpenger.soknad.orkestrator.PostgresDataSourceBuilder.dataSource
 import no.nav.dagpenger.soknad.orkestrator.PostgresDataSourceBuilder.runMigration
 import no.nav.dagpenger.soknad.orkestrator.api.internalApi
 import no.nav.dagpenger.soknad.orkestrator.behov.BehovMottak
 import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory
-import no.nav.dagpenger.soknad.orkestrator.config.Configuration.config
 import no.nav.dagpenger.soknad.orkestrator.config.apiKonfigurasjon
 import no.nav.dagpenger.soknad.orkestrator.opplysning.db.OpplysningRepositoryPostgres
 import no.nav.dagpenger.soknad.orkestrator.søknad.SøknadMottak
@@ -54,7 +52,6 @@ internal class ApplicationBuilder(configuration: Map<String, String>) : RapidsCo
 
     override fun onStartup(rapidsConnection: RapidsConnection) {
         logger.info { "Starter dp-soknad-orkestrator" }
-        if (config["CLEAN_ON_STARTUP"] == "true") clean()
         Database.connect(datasource = dataSource)
             .also {
                 logger.info { "Koblet til database ${it.name}}" }
