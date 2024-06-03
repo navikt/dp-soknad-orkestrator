1d94b89bf5129bf9cfdb3258155ef3182da13ba5
John Martin Lindseth <john.martin.lindseth@nav.no>
2024-06-03 10:12:19 +0200

    Svar OK på kall til roten av apiet
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/api/InternalApi.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/api/InternalApi.kt
@@ -1,13 +1,18 @@
 package no.nav.dagpenger.soknad.orkestrator.api
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
 fun Application.internalApi() {
     routing {
+        get("/") {
+            call.respond(HttpStatusCode.OK)
+        }
         get("/isAlive") {
             call.respondText("Alive")
         }
