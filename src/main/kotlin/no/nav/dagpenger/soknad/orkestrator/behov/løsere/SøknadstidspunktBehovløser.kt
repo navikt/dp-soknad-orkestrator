135ee2266c824a48e3085d04b997e117ce6613f0
John Martin Lindseth <john.martin.lindseth@nav.no>
2024-06-03 17:00:05 +0200

    Svar på behov for søknadstidspunkt med dato
    Opplysningen lagres som en ZonedDateTime av typen Tekst, men det er
    behov for dato i stedet.

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/løsere/SøknadstidspunktBehovløser.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/løsere/SøknadstidspunktBehovløser.kt
@@ -2,8 +2,11 @@ package no.nav.dagpenger.soknad.orkestrator.behov.løsere
 
 import no.nav.dagpenger.soknad.orkestrator.behov.Behovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.Søknadstidspunkt
+import no.nav.dagpenger.soknad.orkestrator.behov.Behovmelding
 import no.nav.dagpenger.soknad.orkestrator.opplysning.db.OpplysningRepository
 import no.nav.helse.rapids_rivers.RapidsConnection
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class SøknadstidspunktBehovløser(
     rapidsConnection: RapidsConnection,
@@ -11,4 +14,17 @@ class SøknadstidspunktBehovløser(
 ) : Behovløser(rapidsConnection, opplysningRepository) {
     override val behov = Søknadstidspunkt.name
     override val beskrivendeId = "søknadstidspunkt"
+
+    override fun løs(behovmelding: Behovmelding) {
+        val søknadstidspunkt =
+            opplysningRepository.hent(beskrivendeId, behovmelding.ident, behovmelding.søknadId)?.svar ?: throw IllegalStateException(
+                "Fant ingen opplysning med beskrivendeId: $beskrivendeId " +
+                    "og kan ikke svare på behov $behov for søknad med id: ${behovmelding.søknadId}",
+            )
+
+        val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+        val søknadsDato = ZonedDateTime.parse(søknadstidspunkt as String, formatter).toLocalDate()
+
+        publiserLøsning(behovmelding, søknadsDato)
+    }
 }
--- src/test/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/løsere/SøknadstidspunktBehovløserTest.kt
+++ src/test/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/løsere/SøknadstidspunktBehovløserTest.kt
@@ -7,6 +7,8 @@ import no.nav.dagpenger.soknad.orkestrator.opplysning.Opplysning
 import no.nav.dagpenger.soknad.orkestrator.opplysning.datatyper.Tekst
 import no.nav.dagpenger.soknad.orkestrator.utils.InMemoryOpplysningRepository
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 import kotlin.test.Test
 
@@ -19,11 +21,15 @@ class SøknadstidspunktBehovløserTest {
 
     @Test
     fun `Behovløser publiserer løsning på behov Søknadstidspunkt`() {
+        val søknadsTidspunkt = ZonedDateTime.parse("2024-01-01T10:15:30+02:00")
+        val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+        val formatertTidspunkt = søknadsTidspunkt.format(formatter)
+
         val opplysning =
             Opplysning(
                 beskrivendeId = behovløser.beskrivendeId,
                 type = Tekst,
-                svar = "false",
+                svar = formatertTidspunkt,
                 ident = ident,
                 søknadId = søknadId,
             )
@@ -31,7 +37,7 @@ class SøknadstidspunktBehovløserTest {
         opplysningRepository.lagre(opplysning)
         behovløser.løs(lagBehovmelding(ident, søknadId, BehovløserFactory.Behov.Søknadstidspunkt))
 
-        testRapid.inspektør.message(0)["@løsning"]["Søknadstidspunkt"]["verdi"].asText() shouldBe "false"
+        testRapid.inspektør.message(0)["@løsning"]["Søknadstidspunkt"]["verdi"].asText() shouldBe "2024-01-01"
     }
 
     @Test
