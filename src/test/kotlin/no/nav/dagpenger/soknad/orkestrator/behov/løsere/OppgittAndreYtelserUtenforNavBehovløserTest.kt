bbe6751490415d35501bda3c140de5cecf9c211a
celinetendam <celine.ten.dam@nav.no>
2024-08-06 11:31:51 +0200

    Rename behovet til forventet navn "OppgittAndreYtelserUtenforNav".

--- src/test/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/løsere/OppgittAndreYtelserUtenforNavBehovløserTest.kt
+++ src/test/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/løsere/OppgittAndreYtelserUtenforNavBehovløserTest.kt
@@ -2,7 +2,7 @@ package no.nav.dagpenger.soknad.orkestrator.behov.løsere
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
-import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.AndreYtelser
+import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.OppgittAndreYtelserUtenforNav
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Opplysning
 import no.nav.dagpenger.soknad.orkestrator.opplysning.datatyper.Boolsk
 import no.nav.dagpenger.soknad.orkestrator.utils.InMemoryOpplysningRepository
@@ -10,15 +10,15 @@ import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import java.util.UUID
 import kotlin.test.Test
 
-class AndreYtelserBehovløserTest {
+class OppgittAndreYtelserUtenforNavBehovløserTest {
     val opplysningRepository = InMemoryOpplysningRepository()
     val testRapid = TestRapid()
-    val behovløser = AndreYtelserBehovløser(testRapid, opplysningRepository)
+    val behovløser = OppgittAndreYtelserUtenforNavBehovløser(testRapid, opplysningRepository)
     val ident = "12345678910"
     val søknadId = UUID.randomUUID()
 
     @Test
-    fun `Behovløser publiserer løsning på behov AndreYtelser`() {
+    fun `Behovløser publiserer løsning på behov OppgittAndreYtelserUtenforNav`() {
         val opplysning =
             Opplysning(
                 beskrivendeId = behovløser.beskrivendeId,
@@ -29,9 +29,15 @@ class AndreYtelserBehovløserTest {
             )
 
         opplysningRepository.lagre(opplysning)
-        behovløser.løs(lagBehovmelding(ident, søknadId, AndreYtelser))
+        behovløser.løs(
+            lagBehovmelding(
+                ident,
+                søknadId,
+                OppgittAndreYtelserUtenforNav,
+            ),
+        )
 
-        testRapid.inspektør.message(0)["@løsning"]["AndreYtelser"]["verdi"].asBoolean() shouldBe false
+        testRapid.inspektør.message(0)["@løsning"]["OppgittAndreYtelserUtenforNav"]["verdi"].asBoolean() shouldBe false
     }
 
     @Test
@@ -41,7 +47,7 @@ class AndreYtelserBehovløserTest {
                 lagBehovmelding(
                     ident,
                     søknadId,
-                    AndreYtelser,
+                    OppgittAndreYtelserUtenforNav,
                 ),
             )
         }
