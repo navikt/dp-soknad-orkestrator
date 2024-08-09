a85d2b7955ccb56a016b5865634760b7823bf68c
celinetendam <celine.ten.dam@nav.no>
2024-08-09 14:14:47 +0200

    Legg til indeks på søknadId i OpplysningTabell.

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/opplysning/db/Tabeller.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/opplysning/db/Tabeller.kt
@@ -11,7 +11,7 @@ object OpplysningTabell : IntIdTable("opplysning") {
     val beskrivendeId: Column<String> = varchar("beskrivende_id", 255)
     val type: Column<String> = text("type")
     val ident: Column<String> = varchar("ident", 11)
-    val søknadId: Column<UUID> = uuid("soknad_id")
+    val søknadId: Column<UUID> = uuid("soknad_id").index()
 }
 
 object TekstTabell : IntIdTable("tekst") {
