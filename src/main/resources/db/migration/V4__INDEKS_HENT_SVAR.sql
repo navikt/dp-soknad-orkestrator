3a7f8e068bb7d60c58c31559453c80bb23bb6f87
celinetendam <celine.ten.dam@nav.no>
2024-08-14 10:51:52 +0200

    Legg til indeks på svartabeller.
    Co-authored-by: John Martin Lindseth <john.martin.lindseth@nav.no>

---
+++ src/main/resources/db/migration/V4__INDEKS_HENT_SVAR.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_flervalg_svar ON flervalg_svar (flervalg_id);
+CREATE INDEX idx_arbeidsforhold_svar ON arbeidsforhold_svar (arbeidsforhold_id);
+CREATE INDEX idx_eøs_arbeidsforhold_svar ON eøs_arbeidsforhold_svar (arbeidsforhold_id);
+CREATE INDEX idx_egen_næring_svar ON egen_næring_svar (egen_næring_id);
+CREATE INDEX idx_barn_svar ON barn_svar (barn_id);
