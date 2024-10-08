65d3477c190a10f35915e14d5678e136030ef619
celinetendam <celine.ten.dam@nav.no>
2024-10-08 09:46:27 +0200

    Disable WRITE_DATES_AS_TIMESTAMPS i objectmapper.
    Co-authored-by: John Martin Lindseth <john.martin.lindseth@nav.no>

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/config/ObjectMapper.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/config/ObjectMapper.kt
@@ -2,6 +2,7 @@ package no.nav.dagpenger.soknad.orkestrator.config
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 
@@ -15,4 +16,5 @@ val objectMapper =
 
         registerModules(JavaTimeModule())
         configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+        configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
     }
