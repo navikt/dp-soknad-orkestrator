aff5a959c6c8cedf969d2b9458ec37f2bb6e9c97
celinetendam <celine.ten.dam@nav.no>
2024-11-13 12:52:40 +0100

    Oppgrader versjonskatalogen med nødvendige endringer.

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/SøknadApi.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/SøknadApi.kt
@@ -2,17 +2,15 @@ package no.nav.dagpenger.soknad.orkestrator.søknad
 
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
-import io.ktor.server.application.ApplicationCall
-import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
+import io.ktor.server.routing.RoutingContext
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.put
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
-import io.ktor.util.pipeline.PipelineContext
 import no.nav.dagpenger.soknad.orkestrator.api.auth.ident
 import no.nav.dagpenger.soknad.orkestrator.opplysning.Svar
 import java.util.UUID
@@ -54,6 +52,8 @@ internal fun Application.søknadApi(søknadService: SøknadService) {
     }
 }
 
-internal fun PipelineContext<Unit, ApplicationCall>.søknadUuid() =
-    call.parameters["søknadId"].let { UUID.fromString(it) }
-        ?: throw IllegalArgumentException("Må ha med id i parameter")
+private fun RoutingContext.søknadUuid() =
+    (
+        call.parameters["søknadId"].let { UUID.fromString(it) }
+            ?: throw IllegalArgumentException("Må ha med søknadId i parameter")
+    )
