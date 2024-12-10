badc20752ca7b97951ed470498ec58d3f171b31c
John Martin Lindseth <john.martin.lindseth@nav.no>
2024-12-10 15:21:50 +0100

    Legg til api for forelegging av inntekt

---
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/inntekt/InntektApi.kt
@@ -0,0 +1,53 @@
+package no.nav.dagpenger.soknad.orkestrator.inntekt
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.auth.Authentication
+import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.routing.RoutingContext
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
+import no.nav.dagpenger.soknad.orkestrator.api.auth.AuthFactory.tokenX
+import java.util.UUID
+
+internal fun Application.inntektApi(inntektService: InntektService) {
+    install(Authentication) {
+        jwt("tokenX") { tokenX() }
+    }
+
+    routing {
+        get("/") { call.respond(HttpStatusCode.OK) }
+
+        authenticate("tokenX") {
+            route("/inntekt") {
+                get("/{søknadId}/forelagtopplysning") {
+                    val søknadId = søknadUuid()
+
+                    val inntekt = inntektService.hentForelagtOpplysning(søknadId)
+
+                    call.respond(HttpStatusCode.OK, inntekt)
+                }
+
+                post("/{søknadId}/svar") {
+                    val søknadId = søknadUuid()
+
+                    inntektService.lagreSvar(søknadId, call.receive())
+
+                    call.respond(HttpStatusCode.OK)
+                }
+            }
+        }
+    }
+}
+
+private fun RoutingContext.søknadUuid() =
+    (
+        call.parameters["søknadId"].let { UUID.fromString(it) }
+            ?: throw IllegalArgumentException("Må ha med søknadId i parameter")
+    )
