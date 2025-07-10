b6a7f23adad7063459be9e7658959ca2f2aa4ba7
Arselan.Sultani <arselan.sultani@nav.no>
2025-07-10 10:19:32 +0200

    Legg til opprettet kolonne i seksjonstabellen
    Co-authored-by: John Martin Lindseth <john.martin.lindseth@nav.no>

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/seksjon/SeksjonTabell.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/seksjon/SeksjonTabell.kt
@@ -4,13 +4,16 @@ import no.nav.dagpenger.soknad.orkestrator.config.objectMapper
 import no.nav.dagpenger.soknad.orkestrator.søknad.db.SøknadTabell
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.javatime.datetime
 import org.jetbrains.exposed.sql.json.jsonb
+import java.time.LocalDateTime
 import java.util.UUID
 
 object SeksjonTabell : UUIDTable("seksjon") {
     val seksjonId: Column<String> = text("seksjon_id")
     val søknadId: Column<UUID> = uuid("soknad_id").references(SøknadTabell.søknadId)
     val json: Column<String> = jsonb("json", { serializeSøknadData(it) }, { it })
+    val opprettet: Column<LocalDateTime> = datetime("opprettet").default(LocalDateTime.now())
 }
 
 private fun serializeSøknadData(søknadData: String): String = objectMapper.writeValueAsString(søknadData)
