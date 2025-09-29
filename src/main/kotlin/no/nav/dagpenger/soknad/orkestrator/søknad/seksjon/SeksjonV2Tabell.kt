c0958fcaf953e8ca12cd3a5f36d6d2d3c4c8556f
Vegard Skjefstad <vegard.skjefstad@nav.no>
2025-09-29 08:11:02 +0200

    Migrer seksjon_v2.json fra jsonb til json

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/seksjon/SeksjonV2Tabell.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/seksjon/SeksjonV2Tabell.kt
@@ -5,14 +5,14 @@ import no.nav.dagpenger.soknad.orkestrator.søknad.db.SøknadTabell
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.javatime.datetime
-import org.jetbrains.exposed.sql.json.jsonb
+import org.jetbrains.exposed.sql.json.json
 import java.time.LocalDateTime
 import java.util.UUID
 
 object SeksjonV2Tabell : IntIdTable("seksjon_v2") {
     val seksjonId: Column<String> = text("seksjon_id")
     val søknadId: Column<UUID> = uuid("soknad_id").references(SøknadTabell.søknadId)
-    val json: Column<String> = jsonb("json", { serializeSøknadData(it) }, { it })
+    val json: Column<String> = json("json", { serializeSøknadData(it) }, { it })
     val opprettet: Column<LocalDateTime> = datetime("opprettet").default(LocalDateTime.now())
 }
 
