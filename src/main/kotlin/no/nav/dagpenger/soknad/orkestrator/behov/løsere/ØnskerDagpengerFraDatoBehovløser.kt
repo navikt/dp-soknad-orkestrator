49836cebab588ab594d446c45376335a10cb0352
John Martin Lindseth <john.martin.lindseth@nav.no>
2025-11-26 09:00:39 +0100

    Hent data fra ny søknad i ØnskerDagpengerFraDatoBehovløser (#272)
    Funksjonelt tilsvarende det som finnes fra før, der vi sjekker både vanlig og gjenopptak.

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/løsere/ØnskerDagpengerFraDatoBehovløser.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/behov/løsere/ØnskerDagpengerFraDatoBehovløser.kt
@@ -1,9 +1,12 @@
 package no.nav.dagpenger.soknad.orkestrator.behov.løsere
 
+import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDate
+import com.github.navikt.tbd_libs.rapids_and_rivers.isMissingOrNull
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import no.nav.dagpenger.soknad.orkestrator.behov.Behovløser
 import no.nav.dagpenger.soknad.orkestrator.behov.BehovløserFactory.Behov.ØnskerDagpengerFraDato
 import no.nav.dagpenger.soknad.orkestrator.behov.Behovmelding
+import no.nav.dagpenger.soknad.orkestrator.config.objectMapper
 import no.nav.dagpenger.soknad.orkestrator.quizOpplysning.db.QuizOpplysningRepository
 import no.nav.dagpenger.soknad.orkestrator.søknad.db.SøknadRepository
 import no.nav.dagpenger.soknad.orkestrator.søknad.seksjon.SeksjonRepository
@@ -19,16 +22,38 @@ class ØnskerDagpengerFraDatoBehovløser(
         get() = throw NotImplementedError("Overrider løs() og trenger ikke beskrivendeId")
     val beskrivendeIdSøknadsdato = "faktum.dagpenger-soknadsdato"
     val beskrivendeIdGjenopptaksdato = "faktum.arbeidsforhold.gjenopptak.soknadsdato-gjenopptak"
+    val dagpengerFraDatoFelt = "hvilkenDatoSøkerDuDagpengerFra"
+    val gjenopptakFraDatoFelt = "hvilkenDatoSøkerDuGjenopptakFra"
 
     override fun løs(behovmelding: Behovmelding) {
         val svarPåBehov =
             opplysningRepository.hent(beskrivendeIdSøknadsdato, behovmelding.ident, behovmelding.søknadId)?.svar
                 ?: opplysningRepository.hent(beskrivendeIdGjenopptaksdato, behovmelding.ident, behovmelding.søknadId)?.svar
-                ?: throw IllegalStateException(
-                    "Fant ingen opplysning med beskrivendeId: $beskrivendeIdSøknadsdato eller $beskrivendeIdGjenopptaksdato " +
-                        "og kan ikke svare på behov $behov for søknad med id: ${behovmelding.søknadId}",
-                )
 
-        publiserLøsning(behovmelding, svarPåBehov)
+        if (svarPåBehov != null) {
+            return publiserLøsning(behovmelding, svarPåBehov)
+        }
+
+        val seksjonsSvar =
+            seksjonRepository.hentSeksjonsvarEllerKastException(
+                behovmelding.ident,
+                behovmelding.søknadId,
+                "din-situasjon",
+            )
+
+        objectMapper.readTree(seksjonsSvar).let { seksjonssJson ->
+            val dagpengerFraDato = seksjonssJson.findPath(dagpengerFraDatoFelt)
+            val gjenopptakFraDato = seksjonssJson.findPath(gjenopptakFraDatoFelt)
+
+            if (!dagpengerFraDato.isMissingOrNull()) {
+                return publiserLøsning(behovmelding, dagpengerFraDato.asLocalDate())
+            } else if (!gjenopptakFraDato.isMissingOrNull()) {
+                return publiserLøsning(behovmelding, gjenopptakFraDato.asLocalDate())
+            }
+        }
+
+        throw IllegalStateException(
+            "Fant ingen opplysning på behov $behov for søknad med id: ${behovmelding.søknadId}",
+        )
     }
 }
