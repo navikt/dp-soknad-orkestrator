c84b674484c877a0fca9b183c3b4cca10403e9a7
ArselanSultani <arselan.sultani@nav.no>
2026-01-19 16:22:25 +0100

    Journalfør ettersending av melding

--- src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/melding/MeldingOmEttersending.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/orkestrator/søknad/melding/MeldingOmEttersending.kt
@@ -2,19 +2,30 @@ package no.nav.dagpenger.soknad.orkestrator.`søknad`.melding
 
 import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import no.nav.dagpenger.soknad.orkestrator.søknad.Dokument
-import no.nav.dagpenger.soknad.orkestrator.søknad.Søknad
+import no.nav.dagpenger.soknad.orkestrator.søknad.NyEttersendingJournalPost
 import java.util.UUID
 
 class MeldingOmEttersending(
     private val søknadId: UUID,
     private val ident: String,
-    private val søknad: Søknad,
     private val dokumentKravene: List<Dokument>,
 ) {
+    companion object {
+        const val BEHOV = "journalfør_ettersending_av_dokumentasjon"
+    }
+
     fun asMessage(): JsonMessage =
-        JsonMessage.newMessage(
-            søknadId = søknadId,
-            ident = ident,
-            dokumenter = dokumentKravene,
+        JsonMessage.newNeed(
+            behov = listOf(BEHOV),
+            map =
+                mapOf(
+                    "søknadId" to søknadId.toString(),
+                    "ident" to ident,
+                    "type" to "ETTERSENDING_TIL_DIALOG",
+                    BEHOV to
+                        NyEttersendingJournalPost(
+                            dokumenter = dokumentKravene,
+                        ),
+                ),
         )
 }
